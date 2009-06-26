bf5e6d2c21e1d3acbe852b9a780b9d8b133e23b8
John <john@theworkinggroup.ca>
2009-06-26 08:25:19 +0100

    tags

--- lib/seo_helpers.rb
+++ lib/seo_helpers.rb
@@ -21,14 +21,81 @@ module SeoHelpers
       when Hash
         sitename = arguments[:site_name]
         if @title
-          return "#{strip_tags(@title)} - #{sitename}"
+          return %(<title>#{strip_tags(@title)} - #{sitename}</title>)
         else
-          return "#{sitename}"
+          return %(<title>#{sitename}</title>)
         end
       end
     end
   end
 
   module MetaHelper
+    def meta name, content
+      %(<meta name="#{name}" content="#{content}" />)
+    end
+
+    def display_meta options = {:robots => 'all', :language => 'english'}
+      s = ""
+      s += meta('description', options[:description]) unless options[:description].nil?
+      s += meta('keywords', options[:keywords]) unless options[:keywords].nil?
+      s += meta('robots', options[:robots])
+      s += meta('SKYPE_TOOLBAR', 'SKYPE_TOOLBAR_PARSER_COMPATIBLE') if options[:skype] == true
+      s += meta('language', options[:language])
+      return s
+    end
+
+    def display_keywords options = {}
+      if options[:keys].nil?
+        keys = "linux csharp socialnetworks css facebook blog ruby rubyonrails 
+          php5 ajaxian learning javascript python development tsql geek jquery 
+          hosting ubuntu scalable cloudcomputing agilemethods"
+      else
+        keys = options[:keys]
+      end
+      keys = %w(keys).join(',')
+
+      titles = []
+      tags = []
+      cats = []
+
+      # if @articles
+      #   @articles.each { |article| titles.push(article.title) }
+      #   @articles.each { |article| tags.push(article.cached_tag_list) }
+      # 
+      #   if !@article.blank?
+      #     [titles.join(','),
+      #     tags.join(','),
+      #     @article.updated_at.to_s(:month_year)].join(',')
+      #   else
+      #     [titles.join(','),
+      #     tags.join(',')]
+      #   end
+      # 
+      # elsif @article and !@article.new_record?
+      #   @article.categories.each { |category| cats.push(category.title) }
+      # 
+      #   if !@article.updated_at.blank?
+      #     [@article.title,
+      #     @article.cached_tag_list,
+      #     cats.join(','),
+      #     @article.updated_at.to_s(:month_year)].join(',')
+      #   else
+      #     ['',
+      #     '']
+      #   end
+      # 
+      # else
+      # end
+      return meta('keywords', keys)
+    end
+
+    def favicon location = '/favicon.ico'
+      %(<link rel='shortcut icon' href='#{location}' type='image/x-icon' />)
+    end
+
+    def metatype options => {:content = 'text/html', :charset => 'utf-8'}
+      %(<meta http-equiv="content-type" content="#{:options[:type]};charset=#{options[:charset]}" />)
+    end
+    
   end
 end
